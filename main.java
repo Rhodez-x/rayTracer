33e13c205d3c2a04e1c601065ea67aac5a6bb9e6
Rhodez-x <joer0357@yahoo.dk>
2017-03-30 12:28:08 +0200

    Hello world

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
