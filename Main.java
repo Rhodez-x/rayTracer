5cdf8c7d35960646e774ebbcf21709f7b3b0ff83
Rhodez-x <joer0357@yahoo.dk>
2017-04-10 10:58:03 +0200

    Hello work

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 public class Main {
     
     public static void main(String[] args) {
-        Ray myRay = new Ray(new Vector3(-300.0f, 0.0f, 0.0f), new Vector3(1,1,1) );
-        Sphere mySphere = new Sphere(10.0f, 10.0f, 10.0f, 2.0f, new CColor(0, 255, 0, 255));
+        Ray myRay = new Ray(new Vector3(6.0f, 2.0f, 0.0f), new Vector3(-1,1,0) );
+        Sphere mySphere = new Sphere(4.0f, 3.0f, 0.0f, 1.0f, new CColor(0, 255, 0, 255));
+        Sphere mySphere2 = new Sphere(-4.0f, 3.0f, 0.0f, 1.0f, new CColor(0, 255, 0, 255));
         
         for (int i = 0; i < 1; i++) {
             for (int j = 0; j < 1; j++) {
                 System.out.println(mySphere.intersects(myRay, 223424));
+                System.out.println(mySphere2.intersects(myRay, 223424));
             }
         }  
     }
--- Sphere.java
+++ Sphere.java
@@ -27,17 +27,25 @@ public class Sphere implements Shape {
             return false;
         }
         float d2 = Vector3.dot(OC, OC) - (tca * tca);
-        if (d2 > radius ) {
+        if (d2 > radius * radius) {
             return false;
         }
         
-        float thc = (float) Math.sqrt(radius - d2);
+        float thc = (float) Math.sqrt(radius * radius - d2);
         float t0 = tca - thc;
         float t1 = tca + thc;
         if (t0 > t1) {
             float temp = t0;
             t0 = t1;
             t1 = temp;
+        }
+        
+        Vector3 OC2 = Vector3.sub(origin, pos);
+        float a = Vector3.dot(direction, direction);
+        float b = 2 * Vector3.dot(OC2, direction);
+        float c = Vector3.dot(OC2, OC2) - (radius * radius);
+        if (!solveQuadratic(a, b, c, t0, t1)) {
+            return false;
         }
         
         if (t0 < 0) {
@@ -49,4 +57,33 @@ public class Sphere implements Shape {
         System.out.println(t0);
         return true;
     }
+    
+    public boolean solveQuadratic(float a, float b, float c, float x0, float x1) {
+        //float distanceCenter = (b*b) - (4 * c);
+        float discr = b * b - 4 * a * c ;
+        if (discr < 0) {
+            return false;
+        }
+        else if (discr == 0) {
+            x0 = x1 = (float) (-0.5 * b / a);
+        }
+        else {
+            float q = 0;
+            if (b > 0) {
+                q = (float) (-0.5 * (b + (float) Math.sqrt(discr)));
+            }
+            else {
+                q = (float) (-0.5 * (b - (float) Math.sqrt(discr)));
+            }
+            x0 = q / a;
+            x1 = c / q;
+        }
+        if (x0 > x1) {
+            float temp = x0;
+            x0 = x1;
+            x1 = temp;
+        }
+        return true;
+    }
+    
 }
