d2b33bc09f6ba09e5ea30672dd2faeabc8507efd
Rhodez-x <joer0357@yahoo.dk>
2017-04-25 12:27:18 +0200

    Camera added

--- src/core/Main.java
+++ src/core/Main.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.IOException;
 
 import static core.Globals.*;
+import core.world.Camera;
 
 public class Main
 {
@@ -24,6 +25,8 @@ public class Main
         outputRenderedImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 
         Sphere mySphere2 = new Sphere(200.0f, 200.0f, 100.0f, 100.0f, new Color(0, 255, 0));
+        Camera camera = new Camera(new Vector3((float) WIDTH/2, (float) HEIGHT/2, -300.0f));
+
         //core.shapes.Sphere mySphere3 = new core.shapes.Sphere(150.0f, 150.0f, 50.0f, 50.0f, new core.math.CColor(0, 255, 0, 255));
         shapeList.add(mySphere2);
         //shapeList.add(mySphere3);
@@ -31,8 +34,9 @@ public class Main
         for (int y = 0; y < HEIGHT; y++)
         {
             for (int x = 0; x < WIDTH; x++)
-            {
-                Ray myRay = new Ray(new Vector3((float) x, (float) y, -30.0f), new Vector3(0, 0, 1));
+            {   
+                Ray myRay = new Ray(camera.startPos, camera.calcDir(new Vector3(x-200, y-200, -100)));
+                System.out.println(camera.calcDir(new Vector3(x, y, 20)).toString());
                 background.paint(x, y, background.color);
                 for (Shape shape : shapeList)
                 {
